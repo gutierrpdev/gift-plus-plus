3e5bfbea6361058cf09835c67d49166ce96c5aef
Michael Kane <michael@mkkorp.com>
2018-12-14 14:45:10 +0000

    Add some basic core type definitions

---
+++ core/index.ts
@@ -0,0 +1,26 @@
+export interface Gift {
+  kind: 'Gift',
+  id: string,
+  museumId: string,
+  to: string,
+  parts: Array<{
+    note: string,
+    photoUrl: string,
+    audioUrl: string,
+  }>,
+}
+
+
+export interface Account {
+  kind: 'Account',
+  id: string,
+  email: string,
+  name: string,
+}
+
+
+export interface Museum {
+  kind: 'Museum',
+  id: string,
+  name: string,
+}
