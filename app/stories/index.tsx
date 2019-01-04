2a96cc50536f31ae68e51355a6005ce311921a93
Michael Kane <michael@mkkorp.com>
2019-01-04 14:17:36 +0000

    meh

--- app/stories/index.tsx
+++ app/stories/index.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-//import { linkTo } from '@storybook/addon-links';
+import { linkTo } from '@storybook/addon-links';
 
 import Welcome from '../src/screens/Welcome';
 
+
 storiesOf('Welcome', module).add('Test', () => <Welcome onStartPressed={action('start pressed')} />);
 
-/* storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
- *
- * storiesOf('Button', module)
- *   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
- *   .add('with some emoji', () => (
- *     <Button onClick={action('clicked')}>
- *       <span role="img" aria-label="so cool">
- *         😀 😎 👍 💯
- *       </span>
- *     </Button>
- *   )); */
+storiesOf('Test', module).add('to Storybook', () => <Welcome onStartPressed={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <button onClick={action('clicked')}>Hello Button</button>)
+  .add('with some emoji', () => (
+    <button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </button>
+  ));
