f24f11474dc2739e6b8f4aa0c18dd713c4f5173b
Darren Lambert <darren.lambert@gmail.com>
2019-02-13 10:38:18 +0000

    Remove macro

--- app/src/components/PanelManager.tsx
+++ app/src/components/PanelManager.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 // import disableScroll from 'disable-scroll';
 
 import Panel from './Panel';
