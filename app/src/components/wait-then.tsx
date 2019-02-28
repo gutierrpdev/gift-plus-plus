6c75fb5488241b4419a382dce3a8759626f1a95b
Darren Lambert <darren.lambert@gmail.com>
2019-02-28 16:14:48 +0000

    Adds initial WaitThen component

---
+++ app/src/components/wait-then.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+/***
+ * Wait an amount of time, and then do a callback
+ */
+
+interface Props {
+  wait: number;
+  andThen: () => void;
+}
+
+const WaitThen: React.FC<Props> = ({wait, andThen}) => {
+
+  function waitAndThen() {
+    if (wait && andThen) {
+      setTimeout(() => {
+        andThen();
+      }, wait * 1000);
+    }
+  }
+
+  return (
+    <>
+    {waitAndThen()}
+    </>
+  );
+
+};
+
+export {
+  WaitThen,
+};
