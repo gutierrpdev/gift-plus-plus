9287e088f2880eb78ea6d2acc9759824954369ea
Darren Lambert <darren.lambert@gmail.com>
2019-03-01 16:53:14 +0000

    Adds vertical bar to the audio player position indicator

--- app/src/components/progress-bar.tsx
+++ app/src/components/progress-bar.tsx
@@ -5,14 +5,26 @@ const ProgressBarInner = styled.div<Props>`
   background-color: white;
   width: ${(props) => props.percentage}%;
   height: 100%;
+  position: relative;
+  border-radius: 10vmin;
+  &:after {
+    content: '';
+    height: 6vmin;
+    width: 1vmin;
+    background-color: white;
+    position: absolute;
+    top: -2.5vmin;
+    right: 0;
+    border-radius: 10vmin;
+  }
 `;
 
 const StyledProgressBar = styled.div`
   background-color: black;
   height: 1.2vw;
   width: 100%;
-  border-radius: 15px;
-  overflow: hidden;
+  border-radius: 10vmin;
+  /* overflow: hidden; */
   margin: 8% 0;
 `;
 
