7a943b4b28b966ce738e2ead158d16bc1395adca
Michael Kane <michael@mkkorp.com>
2019-03-07 10:25:58 +0000

    temporarily adds hardcoded gift

--- api/src/lib/services/gift.ts
+++ api/src/lib/services/gift.ts
@@ -1,5 +1,5 @@
+import * as uuidv5 from 'uuid/v5';
 import * as Knex from 'knex';
-
 
 type Id = string;
 type MuseumId = Id;
@@ -72,6 +72,9 @@ export class GiftService {
    * @param id
    */
   public async findById(id: GiftId): Promise<Gift | null> {
+    // TEMP: Use hardcoded values
+    if (hardcodedGifts.has(id)) return hardcodedGifts.get(id)!;
+
     return this.findByIds([id]).then((gifts) => gifts.length === 0 ? null : gifts[0]);
   }
 
@@ -135,3 +138,36 @@ function tableRowToGift(row: GiftTableRow): Gift {
     parts: JSON.parse(row.parts),
   };
 }
+
+
+/* tslint:disable max-line-length */
+const hardcodedGifts = new Map<GiftId, Gift>();
+
+// 5475400c-684c-515f-8343-b9d14340de9c
+hardcodedGifts.set(uuidv5('https://api.gift.com/gift/test-1', uuidv5.URL), {
+  id: uuidv5('https://api.gift.com/gift/test-1', uuidv5.URL),
+  kind: 'PersonalGift',
+  museumId: uuidv5('https://api.gift.com/museum/test-1', uuidv5.URL),
+  accountId: uuidv5('https://api.gift.com/account/test-1', uuidv5.URL),
+  senderName: 'The sender name',
+  recipientName: 'The recipient name',
+  recipientGreeting: 'https://s3.eu-west-2.amazonaws.com/bt-gift-app/staging/app-uploads/c879827b-3c84-47cc-8ee5-37955449b0e1/a3604a43-866f-463c-aec2-0f8dded8772f-recipientName.m4a',
+  parts: [
+    {
+      photo: 'https://s3.eu-west-2.amazonaws.com/bt-gift-app/staging/app-uploads/c879827b-3c84-47cc-8ee5-37955449b0e1/a3604a43-866f-463c-aec2-0f8dded8772f-part1-photo.jpg',
+      note: 'https://s3.eu-west-2.amazonaws.com/bt-gift-app/staging/app-uploads/c879827b-3c84-47cc-8ee5-37955449b0e1/a3604a43-866f-463c-aec2-0f8dded8772f-part1-clue.m4a',
+      clue: 'Part 1 clue',
+    },
+    {
+      photo: 'https://s3.eu-west-2.amazonaws.com/bt-gift-app/staging/app-uploads/c879827b-3c84-47cc-8ee5-37955449b0e1/a3604a43-866f-463c-aec2-0f8dded8772f-part2-photo.jpg',
+      note: 'https://s3.eu-west-2.amazonaws.com/bt-gift-app/staging/app-uploads/c879827b-3c84-47cc-8ee5-37955449b0e1/a3604a43-866f-463c-aec2-0f8dded8772f-part2-clue.m4a',
+      clue: 'Part 2 clue',
+    },
+    {
+      photo: 'https://s3.eu-west-2.amazonaws.com/bt-gift-app/staging/app-uploads/c879827b-3c84-47cc-8ee5-37955449b0e1/a3604a43-866f-463c-aec2-0f8dded8772f-part3-photo.jpg',
+      note: 'https://s3.eu-west-2.amazonaws.com/bt-gift-app/staging/app-uploads/c879827b-3c84-47cc-8ee5-37955449b0e1/a3604a43-866f-463c-aec2-0f8dded8772f-part3-clue.m4a',
+      clue: 'Part 3 clue',
+    },
+  ],
+});
+/* tslint:enable max-line-length */
