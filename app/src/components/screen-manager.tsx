973067a0cce4896b96a09113034afd07e01fcc7e
Darren Lambert <darren.lambert@gmail.com>
2019-03-20 11:36:49 +0000

    Adds height calculations to ensure all content is visible

--- app/src/components/screen-manager.tsx
+++ app/src/components/screen-manager.tsx
@@ -7,7 +7,8 @@ interface Props {
 
 // Arranges the elements on the screen, using flex
 const ScreenManagerStyle = styled.div<Props>`
-  height: 100vh;
+  height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+  height: calc(var(--vh, 1vh) * 100); // Set in JS
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -19,19 +20,34 @@ const ScreenManagerStyle = styled.div<Props>`
   background-size: cover;
 `;
 
-interface Props {
-  backgroundImageUrl?: string;
-}
-
 const ScreenManager: React.FC<Props> = (props) => {
   return (
-
     <ScreenManagerStyle {...props}>
       {props.children}
     </ScreenManagerStyle>
-
   );
 };
+
+// todo: ideally we would hide the chrome
+// Fix the vh screen height issue
+// Browsers do not consider the browser chrome when calculating viewport height
+// We need to calculate out own to ensure our enstire screen is shown
+function setupScreenHeight() {
+  // Calculate the actual height of the window
+  const vh = window.innerHeight * 0.01;
+  // Set the css variable
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// Hookup the resize event to recalc our values
+window.addEventListener('resize', () => {
+  setupScreenHeight();
+});
+
+// Fire when fully loaded
+window.addEventListener('load', () => {
+  setupScreenHeight();
+});
 
 export {
   ScreenManager,
