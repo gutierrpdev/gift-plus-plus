db11e6352569cf0c5f2696a625760900c5da7780
Darren Lambert <darren.lambert@gmail.com>
2019-05-16 16:01:12 +0100

    Temporarily removes event logging to browser

--- app/src/utils/events/index.ts
+++ app/src/utils/events/index.ts
@@ -10,7 +10,7 @@
 export function track( { name, payload }: {name: string, payload: {}} ) {
   const event = createAppEvent(name, payload);
   // tslint:disable-next-line no-console
-  console.log(event);
+  // console.log(event);
   // todo do something with the event
 }
 
