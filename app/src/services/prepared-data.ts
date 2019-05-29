74ca5f328a3926a5bb5538f6b932073908ffe627
Darren Lambert <darren.lambert@gmail.com>
2019-05-29 10:13:15 +0100

    Updates BH default gift

--- app/src/services/prepared-data.ts
+++ app/src/services/prepared-data.ts
@@ -20,19 +20,19 @@ preparedGifts.set(uuidv5('https://api.thegift.app/gift/brighton-museum-1', uuidv
   recipientGreeting: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/1-second-of-silence.m4a',
   parts: [
     {
-      photo: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-031a7ccc/part-1-photo.6046a68e.jpg',
-      note: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-031a7ccc/part-1-note.c3ff803d.m4a',
-      clue: 'Walk past to the back of the museum and turn around.',
+      photo: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-032bd8ddd/part-1-photo.6046a68f.jpg',
+      note: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-032bd8ddd/part-1-note.c3ff803e.mp3',
+      clue: 'Find the cabinet of japanese inspired furniture.',
     },
     {
-      photo: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-031a7ccc/part-2-photo.863ba709.jpg',
-      note: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-031a7ccc/part-2-note.bd932aeb.m4a',
-      clue: 'Go through the little door next to stairs on the ground floor.',
+      photo: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-032bd8ddd/part-2-photo.863ba708.jpg',
+      note: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-032bd8ddd/part-2-note.bd932aec.mp3',
+      clue: 'Look among the chairs by the  entrance.',
     },
     {
-      photo: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-031a7ccc/part-3-photo.819aeecb.jpg',
-      note: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-031a7ccc/part-3-note.2fa33501.m4a',
-      clue: 'Find the room of paintings off the mezzanine upstairs.',
+      photo: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-032bd8ddd/part-3-photo.819aeecd.jpg',
+      note: 'https://bt-gift.s3.eu-west-2.amazonaws.com/static-content/curated/brighton-032bd8ddd/part-3-note.2fa33502.mp3',
+      clue: 'Search for the glass case of figurines.',
     },
   ],
 });
