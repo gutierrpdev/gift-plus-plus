73d7cdb1fc6cad0b53c02be1174b251c85fe2dc7
Darren Lambert <darren.lambert@gmail.com>
2019-07-11 16:55:33 +0100

    Adds Munch museum background image

--- app/src/components/background-svg.tsx
+++ app/src/components/background-svg.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import { assertNever } from '../utils/helpers';
 import { museum } from '../data';
 import { BgSvgFullScreen } from './svg/bg';
+import { BgOrangeFullScreen } from './bg-orange';
 
 /***
  * Return the background SVG for the current museum
  */
 
 const Background = (museum.slug === 'brighton') ? BgSvgFullScreen
-                   : (museum.slug === 'munch') ? BgSvgFullScreen
+                   : (museum.slug === 'munch') ? BgOrangeFullScreen
                    : assertNever(museum.slug);
 
 
