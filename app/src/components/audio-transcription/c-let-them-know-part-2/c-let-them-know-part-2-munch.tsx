a84567c3080f162f7caa3d9fbcd3d9b2a0c68e11
Nick Tandavanitj <nick@blasttheory.co.uk>
2019-08-01 14:53:52 +0000

    Update c-let-them-know-part-2-munch.tsx

--- app/src/components/audio-transcription/c-let-them-know-part-2/c-let-them-know-part-2-munch.tsx
+++ app/src/components/audio-transcription/c-let-them-know-part-2/c-let-them-know-part-2-munch.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CLetThemKnowPart2TranscriptMunch: React.FC = () => (
   <>
     <h5>Record a message for part 2</h5>
-    <p>Time for your second message, to go along with your photo. When you’re ready, hit record.</p>
+    <p>Time for your second message, to go along with your photo.  I can’t help you on this one – sorry – it’s better if it comes from you. When you’re ready, hit record.</p>
   </>
 );
 
